13af5a656f0c68c37c872cc0114a2cc724fdd221
SkiddedUser <105170279+SkiddedUser@users.noreply.github.com>
2024-09-09 10:22:15 -0500

    Update dlsfgdkjg.lua

--- dlsfgdkjg.lua
+++ dlsfgdkjg.lua
@@ -72,7 +72,7 @@ local function ModesM()
 				SpikesSec=BrickColor.new("Neon orange").Color
 			},
 			Music={
-				{Id=137729204680351,Pitch=1,Volume=1.75}
+				{Id=110016472731145,Pitch=1,Volume=1.75}
 			},
 			Pose={
 				CFrame.Angles(math.rad(-3),math.rad(-15),math.rad(0)),
@@ -1343,7 +1343,7 @@ local function ModesM(...)
 			SpikesSec=BrickColor.new("Neon orange").Color
 		},
 		Music={
-			{Id=137729204680351,Pitch=1,Volume=1.75}
+			{Id=110016472731145,Pitch=1,Volume=1.75}
 		},
 		Pose={
 			CFrame.Angles(math.rad(-3),math.rad(-15),math.rad(0)),
@@ -2606,7 +2606,7 @@ local Modes={
 			SpikesSec=BrickColor.new("Neon orange").Color
 		},
 		Music={
-			{Id=137729204680351,Pitch=1,Volume=1.75}
+			{Id=110016472731145,Pitch=1,Volume=1.75}
 		},
 		Pose={
 			CFrame.Angles(math.rad(-3),math.rad(-15),math.rad(0)),
@@ -3930,7 +3930,7 @@ local Modes={
 			SpikesSec=BrickColor.new("Neon orange").Color
 		},
 		Music={
-			{Id=137729204680351,Pitch=1,Volume=1.75}
+			{Id=110016472731145,Pitch=1,Volume=1.75}
 		},
 		Pose={
 			CFrame.Angles(math.rad(-3),math.rad(-15),math.rad(0)),
